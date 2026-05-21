11c432c8f4a99907d7b535afff17bfdad227252d
censate <144240540+censate@users.noreply.github.com>
2026-05-21 18:07:17 +0300

    error handler

--- src/pages/PostsPage/PostsPage.tsx
+++ src/pages/PostsPage/PostsPage.tsx
@@ -5,16 +5,17 @@ import { usePostsStore } from "../../store/postsStore";
 import "./PostsPage.css";
 
 export const PostsPage = () => {
-  const { posts, pagination, perPage, loading, fetchPosts, setPerPage } =
+  const { posts, pagination, perPage, loading, error, fetchPosts, setPerPage } =
     usePostsStore();
 
   useEffect(() => {
     fetchPosts(1);
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="posts-page">
       <h2 className="posts-page__title">Список постов</h2>
+      {error && <div className="error-message">{error}</div>}
       <PostsTable posts={posts} loading={loading} />
       {posts.length > 0 && (
         <PaginationControl
--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -5,16 +5,17 @@ import { useUsersStore } from "../../store/usersStore";
 import "./UsersPage.css";
 
 export const UsersPage = () => {
-  const { users, pagination, perPage, loading, fetchUsers, setPerPage } =
+  const { users, pagination, perPage, loading, error, fetchUsers, setPerPage } =
     useUsersStore();
 
   useEffect(() => {
     fetchUsers(1);
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="users-page">
       <h2 className="users-page__title">Список пользователей</h2>
+      {error && <div className="error-message">{error}</div>}
       <UsersTable users={users} loading={loading} />
       {users.length > 0 && (
         <PaginationControl
